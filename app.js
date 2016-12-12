505d5c06664871c14a0aa62f90d1fe07f963e192
Eric Richardson <eric.richardson97@gmail.com>
2016-12-12 17:54:07 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express')
+var app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+
+app.get('/webhook', function (req, res) {
+  console.log('Webhook Received')
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
