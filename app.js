7e66f5117e6e648bdb564ea3633efa241d24a73f
Eric Richardson <eric.richardson97@gmail.com>
2016-12-13 10:11:52 -0500

    Added scripts to package.json

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ app.get('/webhook', function (req, res) {
   console.log('Webhook Received')
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(80, function () {
+  console.log('Example app listening on port 80!')
 })
