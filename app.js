607de955e562a5a88207f82626fef3e5e5b5ebbc
Eric Richardson <eric.richardson97@gmail.com>
2016-12-23 18:18:40 -0500

    brrrrftttt

--- app.js
+++ app.js
@@ -21,9 +21,9 @@ app.post('/webhook', function (request, response) {
                 let request = require('request');
                 return request(newurl, function (error, response, body) {
                     if (!error && response.statusCode == 200) {
-                        var obj = JSON.parse(body);
-                        var length = Object.keys(obj).length;
-                        assistant.tell('The number of employees at WillowTree is ' + length + '. <break time="1"/> At least that\'s how many people are on the website.');
+                        let obj = JSON.parse(body);
+                        let length = Object.keys(obj).length;
+                        assistant.tell('<speak>The number of employees at WillowTree is ' + length + '. <break time="1"/> At least that\'s how many people are on the website.</speak>');
                     }
                 });
         }
