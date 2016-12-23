3689f1102869a30bdf4ad4a34ed64b5328f620e7
Eric Richardson <eric.richardson97@gmail.com>
2016-12-23 16:48:48 -0500

    Changed to Response Handler

--- app.js
+++ app.js
@@ -1,28 +1,52 @@
 'use strict';
 
 let ApiAiAssistant = require('actions-on-google').ApiAiAssistant;
-var express = require('express')
-var app = express()
+let express = require('express')
+let app = express()
+let bodyParser = require('body-parser');
 app.set('port', (process.env.PORT || 8080));
+app.use(bodyParser.json({type: 'application/json'}));
 
 
 const EMPLOYEES_INTENT = 'input.employees';
 
-app.post('/webhook', function (req, res) {
-	const assistant = new ApiAiAssistant({request: req, response: res});
+app.post('/webhook', function (request, response) {
+	const assistant = new ApiAiAssistant({request: request, response: response});
 	console.log("Assistant Created"); 
-	let actionMap = new Map();
-	actionMap.set(EMPLOYEES_INTENT, employeeIntent);
-	console.log("About to Handle Request"); 
-	console.log(req.body); 
-	assistant.handleRequest(actionMap);
+	// let actionMap = new Map();
+	// actionMap.set(EMPLOYEES_INTENT, employeeIntent);
+	// console.log("About to Handle Request"); 
+	// console.log(req.body); 
+	// assistant.handleRequest(actionMap);
+
+	function responseHandler (assistant) {
+        // intent contains the name of the intent you defined in the Actions area of API.AI
+        let intent = assistant.getIntent();
+        switch (intent) {
+            case EMPLOYEES_INTENT:
+                let newurl = 'http://api.namegame.willowtreemobile.com/';
+                let request = require('request');
+                request(newurl, function (error, response, body) {
+                    console.log("got response back"); 
+                    if (!error && response.statusCode == 200) {
+                        console.log(body) 
+                        var num = Object.keys(d.shareInfo[i]).length;
+                        assistant.tell("The number of employees at WillowTree is %s", num);
+                    }
+                });
+            break;
+    }
+    assistant.handleRequest(responseHandler);
+}
+// you can add the function name instead of an action map
+assistant.handleRequest(responseHandler);
 	console.log("Request Handled"); 
 });
 
 function employeeIntent (assistant) {
 	console.log("In Employee Intent"); 
-    var newurl = 'http://api.namegame.willowtreemobile.com/';
-    var request = require('request');
+    let newurl = 'http://api.namegame.willowtreemobile.com/';
+    let request = require('request');
 	request(newurl, function (error, response, body) {
 		console.log("got response back"); 
   		if (!error && response.statusCode == 200) {
