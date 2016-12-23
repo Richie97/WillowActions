f690be56d8515d3be4d3a8fbfb90644eadb3c11a
Eric Richardson <eric.richardson97@gmail.com>
2016-12-23 17:04:50 -0500

    This is why I don't javascript

--- app.js
+++ app.js
@@ -12,24 +12,17 @@ const EMPLOYEES_INTENT = 'input.employees';
 
 app.post('/webhook', function (request, response) {
 	const assistant = new ApiAiAssistant({request: request, response: response});
-	console.log("Assistant Created"); 
-	// let actionMap = new Map();
-	// actionMap.set(EMPLOYEES_INTENT, employeeIntent);
-	// console.log("About to Handle Request"); 
-	// console.log(req.body); 
-	// assistant.handleRequest(actionMap);
 
 	function responseHandler (assistant) {
-        // intent contains the name of the intent you defined in the Actions area of API.AI
         let intent = assistant.getIntent();
         switch (intent) {
             case EMPLOYEES_INTENT:
                 let newurl = 'http://api.namegame.willowtreemobile.com/';
                 let request = require('request');
                 request(newurl, function (error, response, body) {
-                    console.log("got response back"); 
                     if (!error && response.statusCode == 200) {
-                        console.log(body) 
+                        console.log(body);
+                        console.log(Object.keys(body).length);
                         var num = Object.keys(body).length;
                         assistant.tell("The number of employees at WillowTree is %s", num);
                     }
@@ -38,24 +31,23 @@ app.post('/webhook', function (request, response) {
     }
     assistant.handleRequest(responseHandler);
 }
-// you can add the function name instead of an action map
 assistant.handleRequest(responseHandler);
 	console.log("Request Handled"); 
 });
 
-function employeeIntent (assistant) {
-	console.log("In Employee Intent"); 
-    let newurl = 'http://api.namegame.willowtreemobile.com/';
-    let request = require('request');
-	request(newurl, function (error, response, body) {
-		console.log("got response back"); 
-  		if (!error && response.statusCode == 200) {
-    		console.log(body) 
-    		var num = Object.keys(d.shareInfo[i]).length;
-        	assistant.tell("The number of employees at WillowTree is %s", num);
-  		}
-	});
-}
+// function employeeIntent (assistant) {
+// 	console.log("In Employee Intent"); 
+//     let newurl = 'http://api.namegame.willowtreemobile.com/';
+//     let request = require('request');
+// 	request(newurl, function (error, response, body) {
+// 		console.log("got response back"); 
+//   		if (!error && response.statusCode == 200) {
+//     		console.log(body) 
+//     		var num = Object.keys(d.shareInfo[i]).length;
+//         	assistant.tell("The number of employees at WillowTree is %s", num);
+//   		}
+// 	});
+// }
 
 
 app.post('/', function (request, response) {
